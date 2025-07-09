9d1887f0b89914bccaa3a1606fadcb83f2f975a7
dued-ai <217680655+dued-ai@users.noreply.github.com>
2025-07-09 10:19:16 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+let Die = 0
+input.onGesture(Gesture.Shake, function () {
+    Die = randint(1, 6)
+    if (Die == 1) {
+    	
+    } else {
+        if (Die == 2) {
+        	
+        } else {
+        	
+        }
+    }
+})
+basic.forever(function () {
+	
+})
